09c2a1ebbbc922732b454948f0c7454562c0cc13
r2road <studior2road@gmail.com>
2021-01-22 04:49:09 +0900

    add project : UltimateBaseball

---
+++ intro/UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UltimateBaseball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------------------------------------------------+");
+            Console.WriteLine("|                궁극의 숫자야구 게임                 |");
+            Console.WriteLine("+-----------------------------------------------------+");
+            Console.WriteLine("| 컴퓨터가 수비수가 되어 세 자릿수를 하나 골랐습니다. |");
+            Console.WriteLine("| 각 숫자는 0~9중에 하나며 중복되는 숫자는 없습니다.  |");
+            Console.WriteLine("| 모든 숫자와 위치를 맞히면 승리합니다.               |");
+            Console.WriteLine("| 숫자와 순서가 둘 다 맞으면 스트라이크입니다.        |");
+            Console.WriteLine("| 숫자만 맞고 순서가 틀리면 볼입니다.                 |");
+            Console.WriteLine("| 숫자가 틀리면 아웃입니다.                           |");
+            Console.WriteLine("+-----------------------------------------------------+");
+        }
+    }
+}
