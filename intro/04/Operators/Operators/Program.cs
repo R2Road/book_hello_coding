872ef3c44a742d5a4cd9aca2c4351cf7f1a66e85
r2road <studior2road@gmail.com>
2021-01-25 22:41:14 +0900

    4 - 1 : the four fundamental arithmetic operation

--- intro/04/Operators/Operators/Program.cs
+++ intro/04/Operators/Operators/Program.cs
@@ -6,6 +6,16 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            Console.Write("100 + 10 = ");
+            Console.WriteLine(100 + 10);
+            Console.Write("100 - 10 = ");
+            Console.WriteLine(100 - 10);
+            Console.Write("100 * 10 = ");
+            Console.WriteLine(100 * 10);
+            Console.Write("84 / 10 = ");
+            Console.WriteLine(84 / 10);
+            Console.Write("84 % 10 = ");
+            Console.WriteLine(84 % 10);
         }
     }
 }
