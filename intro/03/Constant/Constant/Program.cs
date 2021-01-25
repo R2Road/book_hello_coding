fcd73f4a5a90488ae0efa8f3af7fd1bc23c34162
r2road <studior2road@gmail.com>
2021-01-25 21:37:13 +0900

    3 - 3 : string

--- intro/03/Constant/Constant/Program.cs
+++ intro/03/Constant/Constant/Program.cs
@@ -6,10 +6,12 @@ namespace Constant
     {
         static void Main(string[] args)
         {
+            Console.Write("바구니 안에 담긴 사과의 개수: ");
             Console.WriteLine(12);
 
             Console.WriteLine(-2);
 
+            Console.Write("사과 바구니의 무게: ");
             Console.WriteLine(1.32);
         }
     }
