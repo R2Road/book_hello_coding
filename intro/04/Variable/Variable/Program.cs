423a12d38525a2e234d5da5dc3dbab9b274d5fcc
r2road <studior2road@gmail.com>
2021-02-02 09:03:07 +0900

    4 - 4 : variable - int, string

--- intro/04/Variable/Variable/Program.cs
+++ intro/04/Variable/Variable/Program.cs
@@ -6,6 +6,10 @@ namespace Variable
     {
         static void Main(string[] args)
         {
+            string appleCountDescription = "바구니 안에 담긴 사과의 개수: ";
+            Console.Write(appleCountDescription);
+            int appleCount = 12;
+            Console.WriteLine(appleCount);
         }
     }
 }
