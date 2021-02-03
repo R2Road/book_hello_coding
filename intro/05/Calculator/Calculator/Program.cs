fd9bc72285d03f4ee8a3ef2066319827c1c4f859
r2road <studior2road@gmail.com>
2021-02-03 11:45:47 +0900

    5 - 1 : addition

--- intro/05/Calculator/Calculator/Program.cs
+++ intro/05/Calculator/Calculator/Program.cs
@@ -6,6 +6,13 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int number1 = 10;
+            int number2 = 5;
+            Console.Write(number1);
+            Console.Write(" + ");
+            Console.Write(number2);
+            Console.Write(" = ");
+            Console.WriteLine(number1 + number2);
         }
     }
 }
