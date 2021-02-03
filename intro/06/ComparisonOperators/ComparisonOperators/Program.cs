91727997d908e2b26b4e65163734f37c6eee33ec
r2road <studior2road@gmail.com>
2021-02-03 12:57:54 +0900

    6 - 2 : compare two numbers

--- intro/06/ComparisonOperators/ComparisonOperators/Program.cs
+++ intro/06/ComparisonOperators/ComparisonOperators/Program.cs
@@ -6,6 +6,23 @@ namespace ComparisonOperators
     {
         static void Main(string[] args)
         {
+            Console.Write("10 > 10 은 ");
+            Console.WriteLine(10 > 10);
+
+            Console.Write("10 >= 10 은 ");
+            Console.WriteLine(10 >= 10);
+
+            Console.Write("5 < 10 은 ");
+            Console.WriteLine(5 < 10);
+
+            Console.Write("5 <= 10 은 ");
+            Console.WriteLine(5 <= 10);
+
+            Console.Write("5 == 4 은 ");
+            Console.WriteLine(5 == 4);
+
+            Console.Write("5 != 4 은 ");
+            Console.WriteLine(5 != 4);
         }
     }
 }
