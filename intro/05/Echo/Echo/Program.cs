e1af74e091146eef904141a8995c50ff2a3b06ca
r2road <studior2road@gmail.com>
2021-02-03 11:50:16 +0900

    5 - 2 : input string

--- intro/05/Echo/Echo/Program.cs
+++ intro/05/Echo/Echo/Program.cs
@@ -6,6 +6,12 @@ namespace Echo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("글자를 입력하고 엔터 키를 누르세요.");
+            string userInput = Console.ReadLine();
+
+            Console.Write("입력한 글은 ");
+            Console.Write(userInput);
+            Console.WriteLine("입니다");
         }
     }
 }
