b7f159e57ae093acacb6f3ab59340e3917f3ede3
r2road <studior2road@gmail.com>
2021-02-05 02:45:26 +0900

    ...

--- intro/06/DoorLock/DoorLock/Program.cs
+++ intro/06/DoorLock/DoorLock/Program.cs
@@ -6,15 +6,15 @@ namespace DoorLock
     {
         static void Main(string[] args)
         {
-            int passcodenumbers1 = 6;
-            int passcodenumbers2 = 2;
+            int passcodeNumbers1 = 6;
+            int passcodeNumbers2 = 2;
 
             Console.WriteLine("첫 번째 숫자를 넣어주세요.");
             int userInput1 = int.Parse(Console.ReadLine());
             Console.WriteLine("두 번째 숫자를 넣어주세요.");
             int userInput2 = int.Parse(Console.ReadLine());
 
-            if (userInput1 == passcodenumbers1 && userInput2 == passcodenumbers2)
+            if (userInput1 == passcodeNumbers1 && userInput2 == passcodeNumbers2)
             {
                 Console.WriteLine("문이 열렸습니다.");
             }
