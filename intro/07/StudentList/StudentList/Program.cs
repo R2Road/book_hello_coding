152a5b17b87d753a52f16d8715d0aec81aa12efa
r2road <studior2road@gmail.com>
2021-02-05 02:55:18 +0900

    7 - 3 : input student info

--- intro/07/StudentList/StudentList/Program.cs
+++ intro/07/StudentList/StudentList/Program.cs
@@ -6,6 +6,33 @@ namespace StudentList
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("학생 숫자를 입력하세요.");
+            int studentCount = int.Parse(Console.ReadLine());
+
+            int[] ages = new int[studentCount];
+            string[] names = new string[studentCount];
+            double[] heights = new double[studentCount];
+
+            Console.WriteLine("몇 번째 학생의 정보를 추가할까요?");
+            int studentNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("나이를 입력하세요.");
+            ages[studentNumber] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("이름을 입력하세요.");
+            names[studentNumber] = Console.ReadLine();
+
+            Console.WriteLine("키를 입력하세요.");
+            heights[studentNumber] = double.Parse(Console.ReadLine());
+
+            Console.Write(studentNumber);
+            Console.WriteLine("번째 학생");
+            Console.Write("이름: ");
+            Console.WriteLine(names[studentNumber]);
+            Console.Write("나이: ");
+            Console.WriteLine(ages[studentNumber]);
+            Console.Write("키: ");
+            Console.WriteLine(heights[studentNumber]);
         }
     }
 }
