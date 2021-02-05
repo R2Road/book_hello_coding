e44c55464cfe6a9c00ccd8cf0c60bf2dbe9e9e4a
r2road <studior2road@gmail.com>
2021-02-06 06:36:35 +0900

    9 - 2 : for

--- intro/09/DoorLock_6Num_For/DoorLock_6Num_For/Program.cs
+++ intro/09/DoorLock_6Num_For/DoorLock_6Num_For/Program.cs
@@ -13,23 +13,28 @@ namespace DoorLock_6Num_For
 
             while (true)
             {
-                int passcodeIndex = 0;
-                while (passcodeIndex < passcodeLength)
+                for (int passcodeIndex = 0; passcodeIndex < passcodeLength; passcodeIndex++)
                 {
                     Console.Write(passcodeIndex);
                     Console.WriteLine("번째 숫자를 넣어주세요.");
                     userInput[passcodeIndex] = int.Parse(Console.ReadLine());
-                    passcodeIndex = passcodeIndex + 1;
                 }
 
-                if (userInput[0] == passcodeNumbers[0] && userInput[1] == passcodeNumbers[1] && userInput[2] == passcodeNumbers[2] && userInput[3] == passcodeNumbers[3] && userInput[4] == passcodeNumbers[4] && userInput[5] == passcodeNumbers[5])
+                bool isPasswordCorrect = true;
+                for (int passcodeIndex = 0; passcodeIndex < passcodeLength; passcodeIndex++)
+                {
+                    if (userInput[passcodeIndex] != passcodeNumbers[passcodeIndex])
+                    {
+                        isPasswordCorrect = false;
+                        Console.WriteLine("비밀번호가 틀렸습니다.");
+                        break;
+                    }
+                }
+
+                if (isPasswordCorrect)
                 {
                     Console.WriteLine("문이 열렸습니다.");
                     break;
-                }
-                else
-                {
-                    Console.WriteLine("비밀번호가 틀렸습니다.");
                 }
             }
         }
