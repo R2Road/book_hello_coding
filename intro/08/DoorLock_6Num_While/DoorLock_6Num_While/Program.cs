5dfb76d00d443043d6b67a8a810cc9be075abdd9
r2road <studior2road@gmail.com>
2021-02-06 05:39:57 +0900

    8 - 4 : while( true )

--- intro/08/DoorLock_6Num_While/DoorLock_6Num_While/Program.cs
+++ intro/08/DoorLock_6Num_While/DoorLock_6Num_While/Program.cs
@@ -9,7 +9,7 @@ namespace DoorLock_6Num_While
             int[] passcodeNumbers = { 6, 2, 1, 9, 4, 7 };
             int[] userInput = new int[6];
 
-            while (userInput[0] != passcodeNumbers[0] || userInput[1] != passcodeNumbers[1] || userInput[2] != passcodeNumbers[2] || userInput[3] != passcodeNumbers[3] || userInput[4] != passcodeNumbers[4] || userInput[5] != passcodeNumbers[5])
+            while (true)
             {
                 Console.WriteLine("첫 번째 숫자를 넣어주세요.");
                 userInput[0] = int.Parse(Console.ReadLine());
