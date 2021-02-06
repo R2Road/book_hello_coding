d72c199622b1f334729457e0089d4759498fcbce
r2road <studior2road@gmail.com>
2021-02-07 08:02:25 +0900

    10 - 1 : door lock with random

--- intro/10/DoorLock_6Num_Random/DoorLock_6Num_Random/Program.cs
+++ intro/10/DoorLock_6Num_Random/DoorLock_6Num_Random/Program.cs
@@ -6,6 +6,49 @@ namespace DoorLock_6Num_Random
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            int passcodeLength = 6;
+
+            int[] passcodeNumbers = new int[passcodeLength];
+
+            Console.WriteLine("비밀번호: ");
+            for (int i = 0; i < passcodeLength; i++)
+            {
+                passcodeNumbers[i] = random.Next(0, 10);
+                Console.Write(passcodeNumbers[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            int[] userInput = new int[passcodeLength];
+
+            while (true)
+            {
+                for (int userInputNumber = 0; userInputNumber < passcodeLength; userInputNumber++)
+                {
+                    Console.Write(userInputNumber);
+                    Console.WriteLine("번째 숫자를 넣어주세요.");
+                    userInput[userInputNumber] = int.Parse(Console.ReadLine());
+                }
+
+                bool corretcPassword = true;
+                for (int userInputNumber = 0; userInputNumber < passcodeLength; userInputNumber++)
+                {
+                    if (userInput[userInputNumber] != passcodeNumbers[userInputNumber])
+                    {
+                        corretcPassword = false;
+                        Console.WriteLine("비밀번호가 틀렸습니다.");
+                        break;
+                    }
+                }
+
+                if (corretcPassword)
+                {
+                    Console.WriteLine("문이 열렸습니다.");
+                    break;
+                }
+            }
         }
     }
 }
