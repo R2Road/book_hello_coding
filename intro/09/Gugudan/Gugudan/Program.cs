e4867d68e83697b67f63034ea7fb4d7ac0d1bbd9
r2road <studior2road@gmail.com>
2021-02-07 06:49:33 +0900

    9 - 4 : gugudan

--- intro/09/Gugudan/Gugudan/Program.cs
+++ intro/09/Gugudan/Gugudan/Program.cs
@@ -6,6 +6,21 @@ namespace Gugudan
     {
         static void Main(string[] args)
         {
+            for (int i = 2; i <= 9; i++)
+            {
+                Console.Write(i);
+                Console.WriteLine("단");
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write(i);
+                    Console.Write(" x ");
+                    Console.Write(j);
+                    Console.Write(" = ");
+                    Console.Write(i * j);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
